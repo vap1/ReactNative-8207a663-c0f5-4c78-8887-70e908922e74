cdabc031ac8e1799eb42a5b6f80112b4c51856a9
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 18:59:17 +0530

    First Commit

--- src/components/TextInputComponent.tsx
+++ src/components/TextInputComponent.tsx
@@ -6,11 +6,15 @@ const TextInputComponent: React.FC = () => {
   const [text, setText] = useState('');
 
   const handleTextChange = (inputText: string) => {
-    console.log('User entered text:', inputText);
+    console.log('TextInputComponent: handleTextChange - Start');
+    console.log('TextInputComponent: handleTextChange - Input Text:', inputText);
+
     setText(inputText);
+
+    console.log('TextInputComponent: handleTextChange - End');
   };
 
-  console.log('Rendering TextInputComponent');
+  console.log('TextInputComponent: Render - Start');
 
   return (
     <TextInput
@@ -28,8 +32,9 @@ const styles = StyleSheet.create({
     borderColor: 'gray',
     borderWidth: 1,
     paddingHorizontal: 10,
-    marginBottom: 10,
   },
 });
+
+console.log('TextInputComponent: Render - End');
 
 export default TextInputComponent;
