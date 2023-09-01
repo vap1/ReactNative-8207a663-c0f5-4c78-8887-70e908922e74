3e4ed908d254203b7cc265e09f68aacc551e08a6
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 18:59:18 +0530

    First Commit

--- src/components/ListItemComponent.tsx
+++ src/components/ListItemComponent.tsx
@@ -1,70 +1,61 @@
 
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import { User } from '../types/Types';
 
-interface ListItemProps {
-  name: string;
-  email: string;
-  contactInfo?: string;
-  address?: string;
-  profilePicture?: string;
-}
+const ListItemComponent: React.FC<{ user: User }> = ({ user }) => {
+  console.log('ListItemComponent.tsx: Start');
 
-const ListItemComponent: React.FC<ListItemProps> = ({
-  name,
-  email,
-  contactInfo,
-  address,
-  profilePicture,
-}) => {
-  console.log('Rendering ListItemComponent');
-  console.log('Name:', name);
-  console.log('Email:', email);
-  console.log('Contact Info:', contactInfo);
-  console.log('Address:', address);
-  console.log('Profile Picture:', profilePicture);
+  console.log('ListItemComponent.tsx: user', user);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.name}>{name}</Text>
-      <Text style={styles.email}>{email}</Text>
-      {contactInfo && <Text style={styles.contactInfo}>{contactInfo}</Text>}
-      {address && <Text style={styles.address}>{address}</Text>}
-      {profilePicture && (
-        <Image source={{ uri: profilePicture }} style={styles.profilePicture} />
-      )}
+      <Image source={{ uri: user.profilePicture }} style={styles.profilePicture} />
+      <View style={styles.detailsContainer}>
+        <Text style={styles.name}>{user.name}</Text>
+        <Text style={styles.email}>{user.email}</Text>
+        <Text style={styles.contactInfo}>{user.contactInfo}</Text>
+        <Text style={styles.address}>{user.address}</Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'row',
+    alignItems: 'center',
     padding: 10,
     borderBottomWidth: 1,
     borderBottomColor: '#ccc',
-  },
-  name: {
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  email: {
-    fontSize: 16,
-    color: '#888',
-  },
-  contactInfo: {
-    fontSize: 16,
-    marginTop: 5,
-  },
-  address: {
-    fontSize: 16,
-    marginTop: 5,
   },
   profilePicture: {
     width: 50,
     height: 50,
     borderRadius: 25,
-    marginTop: 10,
+    marginRight: 10,
+  },
+  detailsContainer: {
+    flex: 1,
+  },
+  name: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  email: {
+    fontSize: 14,
+    color: '#888',
+  },
+  contactInfo: {
+    fontSize: 14,
+    color: '#888',
+  },
+  address: {
+    fontSize: 14,
+    color: '#888',
   },
 });
+
+console.log('ListItemComponent.tsx: End');
 
 export default ListItemComponent;
