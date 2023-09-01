0ab2e0ba51926511751ccd8300289089d64d5fd2
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 18:59:13 +0530

    First Commit

--- src/apis/UserProfileUpdateApi.ts
+++ src/apis/UserProfileUpdateApi.ts
@@ -2,31 +2,16 @@
 import { UserProfileUpdateRequest, UserProfileUpdateResponse } from '../types/Types';
 
 const updateUserProfile = async (request: UserProfileUpdateRequest): Promise<UserProfileUpdateResponse> => {
-  try {
-    console.log('Updating user profile...');
-    console.log('Request:', request);
+  console.log('UserProfileUpdateApi.ts: Start updateUserProfile');
 
-    // Simulating API call and generating random data
-    const updatedProfile = {
-      name: 'John Doe',
-      contactInfo: 'john.doe@example.com',
-      address: '123 Main St',
-      profilePicture: 'https://example.com/profile.jpg',
-    };
+  // Generate random data for testing
+  const response: UserProfileUpdateResponse = {
+    success: true,
+    message: 'User profile updated successfully',
+  };
 
-    console.log('Updated profile:', updatedProfile);
-
-    return {
-      success: true,
-      message: 'User profile updated successfully',
-    };
-  } catch (error) {
-    console.error('Error updating user profile:', error);
-    return {
-      success: false,
-      message: 'Failed to update user profile',
-    };
-  }
+  console.log('UserProfileUpdateApi.ts: End updateUserProfile');
+  return response;
 };
 
 export default updateUserProfile;
