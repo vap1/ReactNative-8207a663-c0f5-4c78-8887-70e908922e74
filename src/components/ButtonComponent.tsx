c6e9329b9de85109cb6c6a24d641e9607d467842
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 18:53:33 +0530

    First Commit

---
+++ src/components/ButtonComponent.tsx
@@ -0,0 +1,40 @@
+
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+interface ButtonProps {
+  title: string;
+  onPress: () => void;
+}
+
+const ButtonComponent: React.FC<ButtonProps> = ({ title, onPress }) => {
+  const handlePress = () => {
+    console.log('Button pressed');
+    onPress();
+  };
+
+  console.log('Rendering ButtonComponent');
+
+  return (
+    <TouchableOpacity style={styles.button} onPress={handlePress}>
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+
+export default ButtonComponent;
