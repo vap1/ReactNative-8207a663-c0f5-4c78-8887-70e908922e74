e866c83df8482a5b0a9cf9c62dc2f1825d5b07b3
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 18:59:19 +0530

    First Commit

--- src/apis/AdminUserDetailsApi.ts
+++ src/apis/AdminUserDetailsApi.ts
@@ -1,33 +1,30 @@
 
 import { AdminUserDetailsRequest, AdminUserDetailsResponse, User } from '../types/Types';
 
+console.log('AdminUserDetailsApi.ts: Start');
+
 export const getAdminUserDetails = async (request: AdminUserDetailsRequest): Promise<AdminUserDetailsResponse> => {
-  try {
-    console.log('Step 1: Starting API call to retrieve admin user details');
-    console.log('Request:', request);
+  console.log('AdminUserDetailsApi.ts: getAdminUserDetails - Start');
 
-    // Simulating API call and generating random data
-    const users: User[] = generateRandomUsers();
-
-    console.log('Step 2: API call successful');
-    console.log('Response:', users);
-
-    return {
-      success: true,
-      users,
+  // Simulating API call and generating random data
+  const users: User[] = [];
+  for (let i = 0; i < 10; i++) {
+    const user: User = {
+      name: `User ${i + 1}`,
+      email: `user${i + 1}@example.com`,
+      contactInfo: `Contact Info ${i + 1}`,
+      address: `Address ${i + 1}`,
+      profilePicture: `https://example.com/profiles/user${i + 1}.jpg`,
     };
-  } catch (error) {
-    console.error('Error occurred while retrieving admin user details:', error);
-
-    return {
-      success: false,
-      users: [],
-    };
+    users.push(user);
   }
+
+  const response: AdminUserDetailsResponse = {
+    users,
+  };
+
+  console.log('AdminUserDetailsApi.ts: getAdminUserDetails - End');
+  return response;
 };
 
-// Helper function to generate random user data
-const generateRandomUsers = (): User[] => {
-  // Generate random user data here
-  return [];
-};
+console.log('AdminUserDetailsApi.ts: End');
